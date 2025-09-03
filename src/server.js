7c07396648cc1aa50b375748afd88410cc7d6f67
DigitalHealthSBH <digitalhealth.sbh2@gmail.com>
2025-09-03 10:56:43 +0700

    sbhoncloud

--- src/server.js
+++ src/server.js
@@ -3,20 +3,40 @@ import cors from '@fastify/cors';
 import dotenv from 'dotenv';
 import ajvCompiler from '@fastify/ajv-compiler';
 
-import patientRoutes from './patients/routes.js';
+import patientRoutes from './hie/routes.js';
+import sbhoncloudRoutes from './sbhoncloud/controller.js';
+
+import vitalRoutes from './vital/routes.js';
+import { startVitalTimer } from './vital/vitalTimer.js';
+
 
 dotenv.config();
 
-const app = Fastify({ logger: true });
+const app = Fastify({
+  logger: true,
+  ajv: {
+    customOptions: {
+      strict: true,
+      removeAdditional: false
+    }
+  }
+});
 
 async function start() {
   await app.register(cors, { origin: '*' });
 
-  app.register(patientRoutes);
+  app.register(patientRoutes, { prefix: '/hie' });
+
+  app.register(sbhoncloudRoutes, { prefix: '/sbhoncloud' });
+
+  app.register(vitalRoutes, { prefix: '/vitalsign' });
 
   const port = process.env.PORT || 3002;
   await app.listen({ port, host: '0.0.0.0' });
   app.log.info(`✅ Server running on http://localhost:${port}`);
+
+    //startVitalTimer(); 
+
 }
 
 start().catch(err => {
